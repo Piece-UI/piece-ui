561eb0e6b15be0d70bc292e8c3836db4cfebfbf7
ayushman76 <avenuedesign768@gmail.com>
2024-12-24 07:32:01 +0530

    Emotion Css added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Button from './components/Button/Button'
 
 function App() {
   return (
-    <Button label="Click Me" className='btn-primary' />
+    <Button label="Click Me" className='btn-primary' varient='primary' />
   );
 }
 
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
-const Button = ({ label, onClick, type = 'button' }) => (
-  <button type={type} onClick={onClick} className="btn">
+const ButtonPiece = styled.button`
+    background-color: ${({varient}) => (varient === 'primary') ? 'blue' : 'gray'};
+    color:white;
+    border:none;
+    padding: 10px 20px;
+    cursor: pointer;
+`;
+
+const Button = ({ label, onClick, type = 'button', varient}) => (
+  <ButtonPiece type={type} onClick={onClick} className="btn" varient={varient}>
     {label}
-  </button>
+  </ButtonPiece>
 );
 
 export default Button;
