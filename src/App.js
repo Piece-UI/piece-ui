84f86b8e22042480b7dc7c57aadc22de7d4010d6
avinash <pavinash469@gmail.com>
2024-12-25 19:47:18 +0530

    card skelton added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Button from './components/Button/Button'
+import Card from './components/cardComponent/Card';
 
 function App() {
   return (
+    <>
     <Button label="Click Me" className='btn-primary' varient='primary' />
+    <Card></Card>
+    </>
+
   );
 }
 
---
+++ src/components/cardComponent/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = ()=>{
+    return(
+        <>
+            <div className="card">
+                <div>dfghjkl;</div>
+                
+            </div>
+        </>
+    )
+}
+
+export default Card;
