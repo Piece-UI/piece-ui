ce66964164b886217e41d221d2695a85d70d61ae
ayushman76 <avenuedesign768@gmail.com>
2024-12-27 19:56:43 +0530

    Component updated

--- src/App.js
+++ src/App.js
@@ -19,11 +19,14 @@ function App() {
     <FlexBox>
           <div>
               <Button 
+                label='Piece Button UI'
                 imgIconBefore={<img src='https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611765.jpg' width='20' height='20' 
                 className='avatar'/>}   
+                iconBefore={<i class="fa-solid fa-user"></i>}
+                iconAfter={<i class="fa-solid fa-chevron-right"></i>} 
                 className='btn-primary' 
                 btnSize='large' 
-                rounded='60px' 
+                rounded='8px' 
                 color='accent--65'
                 disabled={false}
                 onClick={testfn} />
